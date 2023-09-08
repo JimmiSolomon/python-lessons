40514c17e2b89cfee9a10f390ebb50cbbc39537b
Artem Sky <ok123x@yandex.ru>
2023-09-08 16:23:59 +0300

    add my_substr.py

---
+++ my_substr.py
@@ -0,0 +1,10 @@
+def my_substr(string, num):
+	i = 0
+	result = ''
+	while i < num:
+		current_char = string[i]
+		result = result + current_char
+		i += 1
+	return result
+bash: line 1: y: command not found
+
