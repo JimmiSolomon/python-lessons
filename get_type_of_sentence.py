8bc08d2fb7d48af7011ea7b939fefc4769bba1d5
Artem Sky <ok123x@yandex.ru>
2023-09-24 22:40:00 +0300

    add get_type_of_sentence.py

---
+++ get_type_of_sentence.py
@@ -0,0 +1,12 @@
+def get_type_of_sentence(sentence):
+	last_char = sentence[-1]
+
+	if last_char == '?':
+		sentence_type = 'question'
+	elif last_char == '!':
+		sentence_type = 'exclamation'
+	else:
+		sentence_type = 'normal'
+
+	return 'Sentence is ' + sentence_type
+
